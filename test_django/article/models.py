62ea4496252a447e692841cc756e9043c7a6453a
Babkina Sofia <babckina.sofi@yandex.ru>
2024-12-04 22:19:08 +0300

    ЛЗ 24. Cоздаём API для чтения данных.
    Научились создавать API для чтения данных.

--- test_django/article/models.py
+++ test_django/article/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 class Author(models.Model): 
@@ -15,4 +15,12 @@ class Article(models.Model):
  author = models.ForeignKey('Author',null=True, related_name='articles', on_delete=models.CASCADE)
  def __str__(self): 
   return self.title
- 
+
+User = get_user_model()
+class Capital(models.Model):
+    country = models.CharField('country', max_length=150)
+    capital_city = models.CharField('capital', max_length=150)
+    capital_population = models.IntegerField('population')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+      return self.capital_city
--- test_django/article/views.py
+++ test_django/article/views.py
@@ -1,6 +1,11 @@
 from rest_framework import viewsets
+from django.shortcuts import render
 from .models import Article
 from .serializers import ArticleSerializer 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Capital
+from .serializers import CapitalSerializer
 
 # Create your views here.
 
@@ -18,3 +23,18 @@ from .serializers import ArticleSerializer
 class ArticleViewSet(viewsets.ModelViewSet): 
   serializer_class = ArticleSerializer 
   queryset = Article.objects.all()
+
+class GetCapitalInfoView(APIView):
+  def get(self, request):
+    queryset = Capital.objects.all()
+    serializer_for_queryset = CapitalSerializer(instance=queryset, many=True
+    )
+    return Response(serializer_for_queryset.data)
+def main_page(request):
+  list_of_capitals = Capital.objects.all()
+  context = {'list_of_capitals': list_of_capitals}
+  return render(
+      request=request,
+      template_name='capitals/main.html',
+      context=context
+  )
